75bf407d503a15908970e4acfb01c92c809e3ba2
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2023-11-29 04:51:13 +0900

    10.REST API POST(登録)を実装する

--- spring2item/src/main/java/com/udemy/spring2item/service/ItemService.java
+++ spring2item/src/main/java/com/udemy/spring2item/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.udemy.spring2item.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,12 +11,12 @@ import com.udemy.spring2item.model.Item;
 @Service
 public class ItemService {
 	
-	private List<Item> allItems = Arrays.asList(
+	private List<Item> allItems = new ArrayList<> (Arrays.asList(
 			new Item("10001", "ネックレス", "ジュエリー"),
 			new Item("10002", "パーカー", "ファッション"),
 			new Item("10003", "フィエイスクリーム", "ビューティー"),
 			new Item("10004", "サプリ", "ヘルス"),
-			new Item("10005", "ブルーベリー", "フード"));
+			new Item("10005", "ブルーベリー", "フード")));
 	
 	public List<Item> getAllItems(){
 		return allItems;
@@ -28,6 +29,10 @@ public class ItemService {
 			}
 		}
 		return null;
+	}
+	
+	public void addItem(Item item) {
+		allItems.add(item);
 	}
 
 }
