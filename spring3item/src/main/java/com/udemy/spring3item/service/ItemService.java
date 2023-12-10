c3b747a29564b46b98ca5e4a658103db9c734408
MizukiSaitou <r2.f4g.gg9y.rj@gmail.com>
2023-12-11 05:36:04 +0900

    15.キャッシュを使用する

--- spring3item/src/main/java/com/udemy/spring3item/service/ItemService.java
+++ spring3item/src/main/java/com/udemy/spring3item/service/ItemService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 
 import com.udemy.spring3item.model.Item;
@@ -23,27 +26,52 @@ public class ItemService {
 //			new Item("10004", "サプリ", "ヘルス"),
 //			new Item("10005", "ブルーベリー", "フード")));
 	
+	@Cacheable("getItems")
 	public List<Item> getAllItems(){
 		List<Item> allItems = new ArrayList<>();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 		itemRepository.findAll().forEach(allItems::add);
 
 		return allItems;
 	}
 	
+	@Cacheable(value="getItem", key="#p0")
 	public Optional<Item> getItem(Long itemId) {
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 		return itemRepository.findById(itemId);
 	}
 	
+	@CacheEvict(value="getItems", allEntries=true)
 	public void addItem(Item item) {
 		itemRepository.save(item);
 	}
 	
+	@Caching(evict = {
+			@CacheEvict(value="getItems", allEntries=true),
+			@CacheEvict(value="getItem", key="#p0")
+	})
 	public void updateItem(Long itemId, Item item) {
 		if(itemRepository.findById(itemId).get() != null) {
 			itemRepository.save(item);
 		}
 	}
 	
+	@Caching(evict = {
+			@CacheEvict(value="getItems", allEntries=true),
+			@CacheEvict(value="getItem", key="#p0")
+	})
 	public void deleteItem(Long itemId) {
 		itemRepository.deleteById(itemId);
 	}
